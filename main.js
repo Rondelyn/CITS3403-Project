373534bd51a71a5bac45a1ae51fc2ebe76caa4bf
Rondelyn <83633138+Rondelyn@users.noreply.github.com>
2024-04-22 16:55:49 +0800

    Fixed Signup page issues
    - Fixed the site initialization so that both forms don't appear upon entering page
    - Fixed the button so that forms only switch when button is pressed
    -moved the switch functions to main.js
    - Added main.js script tag to all pages
    - Moved form css from login to the main.css
    
    - Button still needs to be appropriately repositioned

---
+++ main.js
@@ -0,0 +1,9 @@
+function showsignup(){
+    document.getElementById('signup form').style.display="block";
+    document.getElementById('login form').style.display="none"; 
+}
+  
+  function showlogin(){
+    document.getElementById('login form').style.display="block";
+    document.getElementById("signup form").style.display="none"; 
+}
