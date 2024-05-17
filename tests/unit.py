fdae06809ec1643d6ddc6acd324fc0135b4ad9a0
lcan2 <165755403+lcan2@users.noreply.github.com>
2024-05-17 17:40:47 +0800

    I think i fixed the tests issue???
    Put the creation of each user/image in the called function

--- tests/unit.py
+++ tests/unit.py
@@ -11,17 +11,11 @@ from app.test_data import add_test_users_to_db, add_test_images_to_db
 class BasicUnitTests(TestCase):
 
     def setUp(self):
-        print("6666666666666")
         testApp = create_app(TestConfig)
-        print("99999999")
         self.app_context = testApp.app_context()
-        print("hfhhf")
         self.app_context.push()
-        print("kjashdkjh")
         db.create_all()
-        print("111111111")
         add_test_users_to_db()
-        print("22222222222")
         add_test_images_to_db()
         
 
@@ -32,28 +26,33 @@ class BasicUnitTests(TestCase):
 
 
 
-    def test_registration_of_user_that_exists(self):
-        with self.assertRaisesRegex(UserCreationError, "user id: " +  "12341234" + " already exists" ):
-            registration('12341234','12341234')
 
     
-    #def test_login_user_does_not_exist(self):
-     #   with self.assertRaisesRegex(UserExistsError, "user id: " +  "898989" + " dose not exist" ):
-      #      login_the_user('898989', '12345678')
+    def test_login_user_does_not_exist(self):
+        print("33333333")
+        with self.assertRaisesRegex(UserExistsError, "user id: " +  "898989" + " dose not exist" ):
+            login_the_user('898989', '12345678')
 
-
+    def test_registration_of_user_that_exists(self):
+        print("111111")
+        with self.assertRaisesRegex(UserCreationError, "user id: " +  "12341234" + " already exists" ):
+            registration('12341234','12341234')
+    
    # def test_login_password_is_wrong(self):
     #    with self.assertRaisesRegex(UserExistsError, "password is incorrect" ):
-     #       login_the_user('12345678', '000000000')
+     #       login_the_user('12341234', '000000000')
 
 
-  #  def test_deleting_post_not_vaild(self):
-   #     with self.assertRaisesRegex(FeedpageUseError, "Not a vaild reason" ):
-     #       reporting_post('12345678', 'I dont like it!!!!!')
+    def test_deleting_post_not_vaild(self):
+        print("2222222")
+        with self.assertRaisesRegex(FeedpageUseError, "Not a vaild reason" ):
+            reporting_post('12345678', 'I dont like it!!!!!')
 
- #   def test_adding_post_with_same_name_to_db(self):
-  #      with self.assertRaisesRegex(NewPostError, "Failed to upload image" ):
-   #         added_image_db('img1.jpg', 'Women', 'hi there', '12345678')
+
+
+    def test_adding_post_with_same_name_to_db(self):
+        with self.assertRaisesRegex(NewPostError, "Failed to upload image" ):
+            added_image_db('img1.jpg', 'Women', 'hi there', '12345678')
 
 
 
